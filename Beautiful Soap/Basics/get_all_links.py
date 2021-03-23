b0c8a1f2ce094f7e2597e3b8b8026698806c9d2c
Hemanth N <hemanthnov2001@gmail.com>
2021-03-23 21:11:15 +0530

    Add files via upload
    This extracts all links from a given webpage

---
+++ Beautiful Soap/Basics/get_all_links.py
@@ -0,0 +1,16 @@
+import requests as rq
+from bs4 import BeautifulSoup
+
+url = input("Enter Link: ")
+if ("https" or "http") in url:
+    data = rq.get(url)
+else:
+    data = rq.get("https://" + url)
+soup = BeautifulSoup(data.text, "html.parser")
+links = []
+for link in soup.find_all("a"):
+    links.append(link.get("href"))
+
+
+with open("LINKS.txt", 'a') as saved:
+    print(links[:10], file=saved)
